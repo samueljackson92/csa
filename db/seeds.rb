94d68dc1a43a93d4a0b7e53607d259e3389b7cb0
Samuel Jackson <samueljackson@outlook.com>
2014-11-21 14:22:56 +0000

    Init repo with project skeleton

---
+++ db/seeds.rb
@@ -0,0 +1,36 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.transaction do
+  1..40.times do |i|
+    user = User.create!(surname: "Surname#{i}",
+                       firstname: "Firstname#{i}",
+                       email: "cwl#{i}@aber.ac.uk",
+                       phone: '01970 622422',
+                       jobs:      true,
+                       grad_year: 1985)
+    UserDetail.create!(login: "cwl#{i}",
+                       password: 'secret',
+                       user: user)
+  end
+  # Create one special admin user
+  user = User.create!(surname: 'Loftus',
+                      firstname: 'Chris',
+                      email: 'cwl@aber.ac.uk',
+                      phone: '01970 622422',
+                      grad_year: 1985)
+  UserDetail.create!(login: 'admin',
+                     password: 'taliesin',
+                     user: user)
+
+  # Create some dummy feeds
+  Feed.create!(name: 'twitter')
+  Feed.create!(name: 'facebook')
+  Feed.create!(name: 'email')
+  Feed.create!(name: 'RSS')
+  Feed.create!(name: 'atom')
+end
