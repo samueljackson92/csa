44eecc58c79a670d9958b74daaacb9868eaf6106
Samuel Jackson <samueljackson@outlook.com>
2014-11-25 09:43:04 +0000

    Tweak test coverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,21 @@
+# Include SimpleConv for converage
+require 'simplecov'
+SimpleCov.start do
+    add_filter '/spec/'
+    add_filter '/config/'
+    add_filter '/lib/'
+    add_filter '/vendor/'
+
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Models', 'app/models'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Views', 'app/views'
+end
+
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-
-# Include SimpleConv for converage
-require 'simplecov'
-SimpleCov.start
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
