2dfee24a3cd796e3527514fcabbe3fe5baca2a53
Samuel Jackson <samueljackson@outlook.com>
2014-11-25 18:26:51 +0000

    Refs #4 up test delay time

--- test/models/broadcast_service_test.rb
+++ test/models/broadcast_service_test.rb
@@ -11,7 +11,7 @@ class BroadcastTest < ActiveSupport::TestCase
   teardown do
     # sleep for a short amount of time after each test
     # this prevents making a request too quickly, leading to accidental failures.
-    sleep 0.3
+    sleep 0.5
   end
 
   test "send broadcast to all feeds" do
