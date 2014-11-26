30ee726114120adc73438c4b1c541aa46f6db7c9
Samuel Jackson <samueljackson@outlook.com>
2014-11-26 00:33:07 +0000

    Refs #4 Get user controller to acceptable level of testing.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,6 +10,21 @@ class UsersControllerTest < ActionController::TestCase
       destroy_dummy_session
   end
 
+  test "should search for user" do
+    get :search, q: 'loftus'
+    assert_response :success
+    assert_template :index
+  end
+
+  test "should search for user using json" do
+    get :search, format: 'json', q: 'loftus'
+    assert_response :success
+
+    users = JSON.parse(@response.body)
+    assert_equal 1, users.length
+    assert_equal 'Loftus', users[0]["surname"]
+  end
+
   test "should get index" do
     get :index
     assert_response :success
@@ -39,13 +54,79 @@ class UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+
+  test "should not show user" do
+    #admins can see everyone by default.
+    #change user id for this test
+    create_dummy_session :two
+    get :show, id: @user
+    assert_redirected_to home_url
+  end
+
+  test "should recover from not finding a user" do
+    get :show, id: -1
+    assert_redirected_to users_url(page: 1)
+  end
+
+  test "should recover from not finding a user with JSON request" do
+    get :show, format: 'json', id: -1
+
+    json_response = JSON.parse(@response.body)
+    assert_response :unprocessable_entity
+    assert_equal "Account no longer exists", json_response["message"]
+  end
+
   test "should get edit" do
     get :edit, id: @user
     assert_response :success
   end
 
+  test "should not be able to edit another users account " do
+    #admins can see everyone by default.
+    #change user id for this test
+    create_dummy_session :two
+    get :edit, id: @user
+    assert_redirected_to home_url
+  end
+
+  test "should be able to edit account if admin" do
+    get :edit, id: users(:two)
+    assert_response :success
+  end
+
   test "should update user" do
-    patch :update, id: @user, user: { email: @user.email, firstname: @user.firstname, grad_year: @user.grad_year, jobs: @user.jobs, phone: @user.phone, surname: @user.surname }
+    patch :update, id: @user, user: { email: @user.email,
+                                      firstname: @user.firstname,
+                                      grad_year: @user.grad_year,
+                                      jobs: @user.jobs,
+                                      phone: @user.phone,
+                                      surname: @user.surname }
+
+    assert_redirected_to "#{user_path(assigns(:user))}?page=1"
+  end
+
+  test "should not be able to update another users account " do
+    #admins can see everyone by default.
+    #change user id for this test
+    create_dummy_session :two
+    patch :update, id: @user, user: { email: @user.email,
+                                      firstname: @user.firstname,
+                                      grad_year: @user.grad_year,
+                                      jobs: @user.jobs,
+                                      phone: @user.phone,
+                                      surname: @user.surname }
+    assert_redirected_to home_url
+  end
+
+  test "should be able to update account if admin" do
+    other_user = users(:two)
+    patch :update, id: other_user, user: { email: other_user.email,
+                                           firstname: other_user.firstname,
+                                           grad_year: other_user.grad_year,
+                                           jobs: other_user.jobs,
+                                           phone: other_user.phone,
+                                           surname: other_user.surname }
+
     assert_redirected_to "#{user_path(assigns(:user))}?page=1"
   end
 
